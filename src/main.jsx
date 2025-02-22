8a39e7fc589d9648e44a5c68569c738a6282e78a
ratul0407 <rajaulislamratul12@gmail.com>
2025-02-22 17:34:19 +0600

    updated the providers

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router}>
-          <Root />
-        </RouterProvider>
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </AuthProvider>
   </StrictMode>
